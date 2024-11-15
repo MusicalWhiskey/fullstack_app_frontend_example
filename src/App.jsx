443cff28a6d01ee83fa09b44976466550a997805
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-11-15 09:35:16 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import NotFound from "./pages/NotFound"
 
 export default function App() {
   return(
     <main>
       <h1>Fullstack App</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </main>
   )
 }
